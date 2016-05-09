958dd0ddd16e6cc0b00c47c0e1589cc0f33321ee
Devon McGrath <devon.mcgrath@gmail.com>
2016-05-09 17:51:07 +1200

    starting the player position function

---
+++ scripts/script.js
@@ -0,0 +1,14 @@
+var start1 = 0; // Player 1 Position
+var start2 = 0; // Player 2 Position
+
+function updatePlayerPosition (){
+	if (player1[start1].className == "active") {
+		;
+	}
+}
+
+function updatePlayerPosition (){
+	if (player1[start2].className == "active") {
+		;
+	}
+}
